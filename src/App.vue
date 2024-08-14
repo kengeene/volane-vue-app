6b290054d731bc38d62029468e34168346d4610b
enkuria <kengeene@gmail.com>
2024-08-14 15:54:59 +0300

    feat: add logic to add task

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,35 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <input type="text" v-model="input">
+    <button @click="addTodo()" style="display: block;">Add Todo Item</button>
+    <hr/>
+    <div>
+      <span v-for="(todo, index) in todos" :key="index" style="display: block;">{{ todo }}</span>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup () {
+    const input = ref('')
+    const todos = ref([])
+    const addTodo = () => {
+      todos.value.push(input.value)
+      input.value = ''
+    }
+
+    return {
+      input,
+      todos,
+      addTodo
+    }
+  }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+</script>
+
+<style lang="scss" scoped>
+
 </style>
